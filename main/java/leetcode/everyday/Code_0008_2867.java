4e077e2ad35e86a88cbff9ba13cce9b6081d30c0
wuxin0011 <2191377759@qq.com>
2024-02-27 19:56:07 +0800

    add day

---
+++ main/java/leetcode/everyday/Code_0008_2867.java
@@ -0,0 +1,80 @@
+package leetcode.everyday;
+
+import leetcode.utils.IoUtil;
+import java.util.*;
+
+/**
+ * @author: wuxin0011
+ * @Description: 统计树中的合法路径数目
+ * @url https://leetcode.cn/problems/count-valid-paths-in-a-tree/description/
+ */
+@SuppressWarnings("all")
+public class Code_0008_2867 {
+
+    public static void main(String[] args) {
+        IoUtil.testUtil(Code_0008_2867.class, "countPaths", "./txt_file/Code_0008_2867.txt");
+    }
+
+
+    private final static int MX = (int) 1e5;
+    private final static boolean[] np = new boolean[MX + 1]; // 质数=false 非质数=true
+
+    static {
+        np[1] = true;
+        for (int i = 2; i * i <= MX; i++) {
+            if (!np[i]) {
+                for (int j = i * i; j <= MX; j += i) {
+                    np[j] = true;
+                }
+            }
+        }
+    }
+
+    public long countPaths(int n, int[][] edges) {
+        List<Integer>[] g = new ArrayList[n + 1];
+        Arrays.setAll(g, e -> new ArrayList<>());
+        for (int[] e : edges) {
+            int x = e[0], y = e[1];
+            g[x].add(y);
+            g[y].add(x);
+        }
+
+        long ans = 0;
+        int[] size = new int[n + 1];
+        List<Integer> nodes = new ArrayList<Integer>();
+        for (int x = 1; x <= n; x++) {
+            if (np[x]) { // 跳过非质数
+                continue;
+            }
+            int sum = 0;
+            for (int y : g[x]) { // 质数 x 把这棵树分成了若干个连通块
+                if (!np[y]) {
+                    continue;
+                }
+                if (size[y] == 0) { // 尚未计算过
+                    nodes.clear();
+                    dfs(y, -1, g, nodes); // 遍历 y 所在连通块，在不经过质数的前提下，统计有多少个非质数
+                    for (int z : nodes) {
+                        size[z] = nodes.size();
+                    }
+                }
+                // 这 size[y] 个非质数与之前遍历到的 sum 个非质数，两两之间的路径只包含质数 x
+                ans += (long) size[y] * sum;
+                sum += size[y];
+            }
+            ans += sum; // 从 x 出发的路径
+        }
+        return ans;
+    }
+
+    private void dfs(int x, int fa, List<Integer>[] g, List<Integer> nodes) {
+        nodes.add(x);
+        for (int y : g[x]) {
+            if (y != fa && np[y]) {
+                dfs(y, x, g, nodes);
+            }
+        }
+    }
+
+
+}
