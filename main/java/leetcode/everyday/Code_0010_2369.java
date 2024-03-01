d6cc32e5c2ef9d7fc8503b242525be787c77263a
wuxin0011 <2191377759@qq.com>
2024-03-01 19:49:46 +0800

    add day

---
+++ main/java/leetcode/everyday/Code_0010_2369.java
@@ -0,0 +1,43 @@
+package leetcode.everyday;
+
+import leetcode.utils.IoUtil;
+
+
+/**
+ * @author: wuxin0011
+ * @Description:  检查数组是否存在有效划分
+ * @url https://leetcode.cn/problems/check-if-there-is-a-valid-partition-for-the-array/
+ */
+@SuppressWarnings("all")
+public class Code_0010_2369 {
+
+    public static void main(String[] args) {
+        IoUtil.testUtil(Code_0010_2369.class, "validPartition", "./txt_file/Code_0010_2369.txt");
+    }
+
+
+    public boolean validPartition(int[] nums) {
+        int n = nums.length;
+        if( n < 1) return false;
+        if( n == 2 ) return nums[0] == nums[1];
+        boolean[] dp = new boolean[n+1];
+        dp[0] = true;
+        dp[2] = nums[1] == nums[0];
+        for(int i = 3;i<=n;i++){
+            if( nums[i-1] == nums[i-2] ){
+                dp[i] |= dp[i-2];
+            }
+            if( nums[i-1] == nums[i-2] && nums[i-2] == nums[i-3] ){
+                dp[i] |= dp[i-3];
+            }
+            if( nums[i-1] - nums[i-2] == 1 &&  nums[i-2] - nums[i-3] == 1 ){
+                dp[i] |= dp[i-3];
+            }
+        }
+        return dp[n];
+
+    }
+
+
+
+}
