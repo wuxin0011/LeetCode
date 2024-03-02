c6b4fb829ad984d430afee961264caa0cf732a6f
wuxin0011 <2191377759@qq.com>
2024-03-02 21:39:01 +0800

    udate top 150

--- main/java/leetcode/top_interview_150/minimum_genetic_mutation/minimum_genetic_mutation.java
+++ main/java/leetcode/top_interview_150/minimum_genetic_mutation/minimum_genetic_mutation.java
@@ -5,65 +5,71 @@ import leetcode.utils.IoUtil;
 /**
  * @author: wuxin0011
  * @Description: 最小基因突变
+ * @url https://leetcode.cn/problems/minimum-genetic-mutation
  */
 public class minimum_genetic_mutation {
     public static void main(String[] args) {
         IoUtil.testUtil(minimum_genetic_mutation.class, "minMutation");
     }
 
+    String endGene;
+    int n;
+    boolean[] vis;
+    String[] bank;
+    int cnt;
+
     public int minMutation(String startGene, String endGene, String[] bank) {
-//        Map<String, Integer> map = new HashMap<>();
-//        for (int i = 0; i < bank.length; i++) {
-//            map.put(bank[i], i);
-//        }
-        boolean f = false;
-        for (int i = 0; i < bank.length; i++) {
-            if (bank[i].equals(endGene)) {
-                f = true;
-                break;
+        this.n = bank.length;
+        this.vis = new boolean[n];
+        this.bank = bank;
+        this.cnt = Integer.MAX_VALUE;
+        this.endGene = endGene;
+        for(int i = 0;i<n;i++){
+            boolean f = check(startGene,bank[i]);
+            //System.out.println("find = " + f + ", idx = " + i);
+            if(f){
+                vis[i] = true;
+                dfs(bank[i],1);
+                vis[i] = false;
             }
         }
-        if (!f) return -1;
-        // 变化后的基因必须位于基因库 bank 中）
-        int cnt = 0;
-        int n = endGene.length();
-        char[] s1 = startGene.toCharArray();
-        char[] s2 = endGene.toCharArray();
-
-        for (int i = 0; i < n; i++) {
-            if (s1[i] == s2[i]) {
-                continue;
-            }
-            if (s2[i] == 'T' && (s1[i] == 'C' || s1[i] == 'G')) {
-                return -1;
-            }
-            if (s2[i] == 'A' && (s1[i] == 'C' || s1[i] == 'G')) {
-                return -1;
-            }
-            if (s2[i] == 'C' && s1[i] != 'G' || s2[i] == 'G' && s1[i] != 'C') {
-                return -1;
-            }
-            s1[i] = s2[i];
-            cnt++;
-
-        }
-        // System.out.println("cnt = " + cnt);
-        return cnt;
+        return cnt == Integer.MAX_VALUE ? -1 : cnt;
     }
 
-
-    public static int getIndex(char c) {
-        switch (c) {
-            case 'A':
-                return 0;
-            case 'C':
-                return 1;
-            case 'G':
-                return 2;
-            case 'T':
-                return 3;
-            default:
-                return -1;
+    public void dfs(String src,int t){
+        System.out.println("cnt => " + t);
+        if(src.equals(endGene)){
+            //System.out.println("find======>");
+            cnt = Math.min(cnt,t);
+            return;
         }
+        for(int i = 0;i<n;i++){
+            if(vis[i]) continue;
+            boolean f = check(src,bank[i]);
+            if(f){
+                //System.out.println("find dfs = " + f + ", idx = " + i);
+                vis[i] = true;
+                dfs(bank[i],t+1);
+                vis[i] = false;
+            }
+        }
+        
+    }
+
+    public static boolean check(String src,String target){
+        if(src.length() != target.length()){
+            return false;
+        }
+        boolean use = false;
+        for(int i = 0;i<src.length();i++){
+            if( src.charAt(i) != target.charAt(i)) {
+                if( use ){
+                    return false;
+                }
+                use = true;
+            }
+        }
+        return true;
+
     }
 }
