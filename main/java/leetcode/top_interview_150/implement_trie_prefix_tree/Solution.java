086623243f80eb559e3245e000f35015e938699b
wuxin0011 <2191377759@qq.com>
2024-04-06 18:05:13 +0800

    完成构造类模板

--- main/java/leetcode/top_interview_150/implement_trie_prefix_tree/Solution.java
+++ main/java/leetcode/top_interview_150/implement_trie_prefix_tree/Solution.java
@@ -1,9 +1,7 @@
 package leetcode.top_interview_150.implement_trie_prefix_tree;
 
-import code_generation.utils.IoUtil;
-import java.util.*;
-
 import code_generation.contest.ParseCodeInfo;
+import code_generation.utils.IoUtil;
 /**
  * @author: wuxin0011
  * @Description:
@@ -15,30 +13,30 @@ public class Solution {
     public static void main(String[] args) {
         IoUtil.testUtil(Trie.class,ParseCodeInfo.ConstructorClass,"in.txt");
     }
-	  
-
-	 public static class Trie { 
-        public Trie() {    
-
-	        
-   		}
-
-        public void insert(String word) {    
-
-	        
-   		}
-
-        public boolean search(String word) {    
-
-	       return false; 
-   		}
-
-        public boolean startsWith(String prefix) {    
-
-	       return false; 
-   		}
 
 
-	}  
+	public static class Trie {
+        public Trie() {
+
+
+		}
+
+        public void insert(String word) {
+
+
+		}
+
+        public boolean search(String word) {
+
+			return false;
+		}
+
+        public boolean startsWith(String prefix) {
+
+			return false;
+		}
+
+
+	}
 
 }
