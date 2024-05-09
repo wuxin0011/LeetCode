9b457bc14e6c43e7b558aad2a71a9ae33bb9e4b5
wuxin0011 <2191377759@qq.com>
2024-05-09 11:14:20 +0800

    add day

--- main/java/leetcode/everyday/day_000/Code_0062_2105.java
+++ main/java/leetcode/everyday/day_000/Code_0062_2105.java
@@ -1,25 +1,49 @@
 package leetcode.everyday.day_000;
 
 import code_generation.utils.IoUtil;
-import java.util.*;
 /**
  * @author: wuxin0011
  * @Description:
- * @url:   https://leetcode.cn/problems/watering-plants-ii
+ * @url: https://leetcode.cn/problems/watering-plants-ii
  * @title: watering-plants-ii
  */
 public class Code_0062_2105 {
 
     public static void main(String[] args) {
-        IoUtil.testUtil(Code_0062_2105.class,"minimumRefill","txt_file\\Code_0062_2105.txt");
+        IoUtil.testUtil(Code_0062_2105.class, "minimumRefill", "txt_file\\Code_0062_2105.txt");
     }
-     
 
-    public int minimumRefill(int[] plants, int capacityA, int capacityB) {    
 
-        return 0; 
-	}
+    public int minimumRefill(int[] plants, int capacityA, int capacityB) {
+        int n = plants.length;
+        int a = 0, b = 0;
+        int ca = capacityA, cb = capacityB;
+        for (int l = 0, r = n - 1; l <= r; l++, r--) {
+            if (r == l && n % 2 == 1) {
+                if (ca >= cb) {
+                    if (plants[l] > ca) {
+                        a++;
+                    }
+                } else {
+                    if (plants[r] > cb) {
+                        b++;
+                    }
+                }
+                break;
+            }
+            if (plants[l] > ca) {
+                ca = capacityA;
+                a++;
+            }
+            if (plants[r] > cb) {
+                cb = capacityB;
+                b++;
+            }
+            ca -= plants[l];
+            cb -= plants[r];
+        }
+        return a + b;
+    }
 
-  
 
 }
--- main/java/leetcode/ox3if/data_struct/heap/base/Solution_0018/Solution.java
+++ main/java/leetcode/ox3if/data_struct/heap/base/Solution_0018/Solution.java
@@ -1,26 +1,54 @@
 package leetcode.ox3if.data_struct.heap.base.Solution_0018;
 
-import code_generation.utils.IoUtil;
-import java.util.*;
 import code_generation.bean.TreeNode;
+import code_generation.utils.IoUtil;
+
+import java.util.TreeSet;
+
 /**
  * @author: wuxin0011
  * @Description:
- * @url:   https://leetcode.cn/problems/QO5KpG
+ * @url: https://leetcode.cn/problems/QO5KpG
  * @title: 二叉搜索树染色
  */
 public class Solution {
 
     public static void main(String[] args) {
-        IoUtil.testUtil(Solution.class,"getNumber","in.txt");
+        IoUtil.testUtil(Solution.class, "getNumber", "in.txt");
     }
-     
 
-    public int getNumber(TreeNode root, int[][] ops) {    
 
-        return 0; 
-	}
+    // 分析
+    // 二叉搜索树，使用中序遍历 转换为有序数组
+    // 可以从后面遍历 因为最终是返回最后一次填充
+    // 可以将最后一次的数据从数组中删除
+    // 返回范围内数据 可以使用 红黑树 TreeSet
+    TreeSet<Integer> set = new TreeSet<>();
 
-  
+    public void dfs(TreeNode node) {
+        if (node == null) return;
+        dfs(node.left);
+        set.add(node.val);
+        dfs(node.right);
+    }
+
+
+    public int getNumber(TreeNode root, int[][] ops) {
+        int cnt = 0, n = ops.length;
+        dfs(root);
+        for (int i = n - 1; i >= 0; i--) {
+            int[] op = ops[i];
+            TreeSet<Integer> o = new TreeSet<>(set.subSet(op[1] - 1, false, op[2] + 1, false));
+            if (o.size() == 0) continue;
+            if (op[0] == 1) {
+                cnt += o.size();
+            }
+            for (Integer idx : o) {
+                set.remove(idx);
+            }
+        }
+        return cnt;
+    }
+
 
 }
