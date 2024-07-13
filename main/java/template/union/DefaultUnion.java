60173a7c8b423fe499e3706a37256465d13b5c55
wuxin0011 <2191377759@qq.com>
2024-07-13 08:44:02 +0800

    update union template

--- main/java/template/union/DefaultUnion.java
+++ main/java/template/union/DefaultUnion.java
@@ -11,49 +11,53 @@ public class DefaultUnion {
     }
 
     // 并查集基础模板
-    // 参考题目
-    static class Union {
-        int MAXN ;
+    // 参考题目 
+    // https://leetcode.cn/problems/count-unreachable-pairs-of-nodes-in-an-undirected-graph
+    public static class Union {
         int[] fa;
-
-        int size = 0;
-
-        public Union(int MAXN) {
-            this.MAXN = MAXN;
-            this.fa = new int[MAXN];
-        }
-
-        public void init(int N) {
-            for (int i = 0; i < N; i++) {
+        int[] s;
+        int size;
+        public Union(int n) {
+            fa = new int[n];
+            s = new int[n];
+            for(int i = 0; i < n;i++) {
                 fa[i] = i;
+                s[i] = 1;
             }
-            size = N;
-        }
-
-        public boolean isSame(int x, int y) {
-            return find(x) == find(y);
-        }
-
-        public boolean union(int x, int y) {
-            int fx = find(x);
-            int fy = find(y);
-            if (fx == fy) {
-                return false;
-            } else {
-                size--;
-                fa[fy] = fx;
-                return true;
-            }
+            size = n;
         }
 
         public int find(int i) {
-            while (i != fa[i]) {
+            while(i != fa[i]) {
                 fa[i] = fa[fa[i]];
                 i = fa[i];
             }
-            return fa[i];
+            return i;
         }
 
 
+        public boolean isSame(int x,int y) {
+            return find(x) == find(y);
+        }
+
+        public boolean union(int x,int y) {
+            x = find(x);
+            y = find(y);
+            if(x == y) {
+                return false;
+            }else {
+                size--;
+                int sx = s[x];
+                int sy = s[y];
+                if(sx >= sy) {
+                    fa[y] = x;
+                    s[x] += s[y];
+                }else {
+                    fa[x] = y;
+                    s[y] += s[x];
+                }
+                return true;
+            }
+        }
     }
 }
