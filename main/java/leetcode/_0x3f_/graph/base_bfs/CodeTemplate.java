a8e48383723f16c657bd45f101e854ae7b6be907
wuxin0011 <2191377759@qq.com>
2024-09-29 10:25:25 +0800

    update

---
+++ main/java/leetcode/_0x3f_/graph/base_bfs/CodeTemplate.java
@@ -0,0 +1,8 @@
+package leetcode._0x3f_.graph.base_bfs;
+
+/**
+ * @author: wuxin0011
+ * @Description:
+ */
+public class CodeTemplate {
+}
---
+++ main/java/template/bitset/readme.md
@@ -0,0 +1,4 @@
+
+### 位图
+
+[基本模板](../../leetcode/_0x3f_/bitwise_operations/base/bitwise_0016/Solution.java)
