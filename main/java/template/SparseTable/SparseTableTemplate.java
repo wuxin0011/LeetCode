d05fd61cc12fb55b4af48acd6e281ef06005f569
wuxin0011 <2191377759@qq.com>
2025-03-14 13:27:27 +0800

    update template

--- main/java/template/SparseTable/SparseTableTemplate.java
+++ main/java/template/SparseTable/SparseTableTemplate.java
@@ -1,13 +1,14 @@
 package template.SparseTable;
 
 
-import code_generation.utils.RandomArrayUtils;
-
 /**
  * St 表 O(1) 查询区间最值问题 解决RMQ 问题
+ * 通常可以用st表代替单调栈求区间等系列问题 这个方法比较莽 但是思路简单
  */
 public class SparseTableTemplate {
 
+
+    // ======================================SparseTable 模板开始 ======================================
     public static class SparseTable {
         @FunctionalInterface
         public interface Operation {
@@ -33,6 +34,7 @@ public class SparseTableTemplate {
             }
         }
 
+        // [l,r]
         public int query(int l, int r) {
             int lt = r - l + 1;
             int q = (int) (Math.floor(Math.log(lt) / Math.log(2)));
@@ -40,7 +42,76 @@ public class SparseTableTemplate {
         }
     }
 
+    // ======================================SparseTable 模板结束 ======================================
 
+
+    // =============================辅助函数=============================
+
+    // 查询 [l,r] 第一次等于x 必须保证 r 为 x 最后一个解 最后查询结果必须严格保证  [pos,end] 严格 等于 x
+    private static int firstEqual(SparseTable st, int l, int r, int x) {
+        int ans = r;
+        int end = r;
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if (st.query(mid, end) == x) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    // 查询 [l,r] 第一次等于x 必须保证 l 为 x 第一个解 最后查询结果必须严格保证  [start,pos] 严格 等于 x
+    private static int lastEqual(SparseTable st, int l, int r, int x) {
+        int ans = r;
+        int start = l;
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if (st.query(start, mid) == x) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+
+    // 查询第一个 >= x 的位置
+    private static int lowerbound(SparseTable st, int l, int r, int x) {
+        int ans = -1;
+        int end = r;
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
+            // >= 替换 <= ,查询 <= 查询第一个小于等于 x
+            if (st.query(l, mid) >= x) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    // 查询最后一个 >= x 的位置
+    private static int upperbound(SparseTable st, int l, int r, int x) {
+        int ans = -1;
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
+            // >= 可以替换 <=
+            if (st.query(mid, r) >= x) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
 
 
 }
--- main/java/template/stack/StackTemplate.java
+++ main/java/template/stack/StackTemplate.java
@@ -1,8 +1,6 @@
 package template.stack;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * 手写栈效率更高同时还能获取栈中不同数据 有时候需要获取栈的 第 x 条数据 这时候系统库就不好用了
@@ -112,13 +110,76 @@ public class StackTemplate {
     // ====================模板结束=====================
 
 
-    public static void main(String[] args) {
-        MyStack<int[]> sk = new MyStack<>(20);
-        List<Integer> a = new ArrayList<>();
-        for (int i = 0; i < 19; i++) {
-            sk.push(new int[]{i, i * i});
+
+
+
+
+    // 常见单调栈 求 a[i] 为最小值的 (l,r)
+    // (l,r) 为开区间 如果是 计算区间长度 (r - l -1） 计算前缀和 (sums[r] - sums[l+1])
+    // 注意如果使用虚拟节点 原来下标遍历需要 + 1 才能对应
+    // -1 表示不存在
+    // =================================单调栈模板开始=====================
+    public static int[][] minArray(int[] a,boolean addDumpNode) {
+        // 如果需要添加哨兵节点
+        if(addDumpNode){
+            int inf = Integer.MAX_VALUE >> 1;
+            int[] dump = new int[a.length + 2];
+            dump[0] = -inf;
+            System.arraycopy(a, 0, dump, 1, a.length);
+            dump[dump.length - 1] = inf;
+            a = dump;
         }
-        System.out.println(Arrays.toString(sk.get(-1)));
-        System.out.println(Arrays.toString(sk.get(-2)));
+        int n = a.length;
+        int[][] ids = new int[n][2];
+        int[] sk = new int[n];
+        int size = 0;
+        for (int i = 0; i < n; i++) {
+            while (size > 0 && a[sk[size - 1]] >= a[i]) {
+                size--;
+            }
+            ids[i][0] = size > 0 ? sk[size - 1] : -1;
+            sk[size++] = i;
+        }
+        size = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            while (size > 0 && a[sk[size - 1]] >= a[i]) {
+                size--;
+            }
+            ids[i][1] = size > 0 ? sk[size - 1] : -1;
+            sk[size++] = i;
+        }
+        return ids;
     }
+
+    // 将 a[i] 转换成负 就是求最大值 的第一个范围了
+    public static int[][] maxArray(int[] a,boolean addDumpNode) {
+        int[] temp = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            temp[i] = -a[i];
+        }
+        return minArray(temp,addDumpNode);
+    }
+
+    // =================================单调栈模板开始=====================
+
+
+
+    // 举例子
+    // https://leetcode.cn/problems/maximum-subarray-min-product/
+    public int maxSumMinProduct(int[] nums) {
+        int[][] ids = minArray(nums,true);
+        int n = nums.length;
+        long ans = 0;
+        int mod = (int)1e9 + 7;
+        long[] sums = new long[n + 4];
+        // 前缀和下标对应
+        for(int i = 1;i <= n;i++) sums[i + 1] = sums[i] + nums[i - 1];
+        for(int i = 1;i <= n;i++){
+            int x = nums[i - 1];
+            ans = Math.max(ans, x * (sums[ids[i][1]] - sums[ids[i][0] + 1]));
+        }
+        ans %= mod;
+        return (int)ans;
+    }
+
 }
