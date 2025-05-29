cbb4ac8c2e26d44bc8c534a6277d6366c8c622fc
wuxin0011 <2191377759@qq.com>
2025-05-29 22:35:26 +0800

    update

---
+++ main/java/template/tips/Tips_2.java
@@ -0,0 +1,10 @@
+package template.tips;
+
+/**
+ *
+ * https://leetcode.cn/problems/minimum-number-of-operations-to-sort-a-binary-tree-by-level/solutions/1965422/by-endlesscheng-97i9/
+ * @author: wuxin0011
+ * @Description: 置换环
+ */
+public class Tips_2 {
+}
